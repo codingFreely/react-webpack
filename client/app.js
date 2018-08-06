a9ae3a022bf22169cde9b7b60e681741e2009a85
Rui Zhang <rui3.zhang@symbio.com>
2018-08-06 16:48:11 +0800

    eslint editorconfig

--- client/app.js
+++ client/app.js
@@ -11,7 +11,7 @@ const render = (Component) => {
         <AppContainer>
             <Component />
         </AppContainer>,
-        root
+        root,
     ) // hydrate会比较客户端渲染内容和服务端渲染内容，如果觉得服务端生成的内容是有问题的，将会在客户端生成的内容替换服务端生成的内容
 }
 
